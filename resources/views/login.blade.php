30a210e7ac770738a8cb8780dcf94f1173eed4f9
Hassan Ali (IT) <hassanali@macl.aero>
2022-11-20 15:19:29 +0500

    front

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,25 +2,36 @@
 
 @section('content')
 
-<div class="container" align="center">
-    
-    <form id = "loginForm" style="max-width:500px">
-        <div class="latestForm">
-            <div class="form-group"> 
-                <label class="form-control-placeholder" for="lastName">Email</label>
-                <input type="text" name="Email" id="Email" class="form-control" value="" required> 
-                <i class="fa fa-user-o iconSet" aria-hidden="true"></i>
+<section>
+    <div class="card float-right bodyPanel" style="width:75%;/*margin:20px 20px 20px 20px;*//*margin-right:22%;*//*background-color:rgba(255,255,255,0.5);*/">
+        <div class="card-body">
+        
+
+            <div class="card float-right contentCard" style="background-color:rgba(255,255,255,0.0);width:500px;margin-top:5%;margin-right:25%">
+                <div class="card-body">
+                    <form id = "loginForm" style="max-width:500px">
+                        <div class="latestForm">
+                            <div class="form-group"> 
+                                <label class="form-control-placeholder" for="lastName">Email</label>
+                                <input type="text" name="Email" id="Email" class="form-control" value="" required> 
+                              
+                            </div>
+                            <div class="form-group"> 
+                                <label class="form-control-placeholder" for="password">Password</label> 
+                                <input type="password" name="Password" id="Password" class="form-control" value="" required> 
+                                
+                            </div> 
+                             
+                        </div> 
+                        <input type="submit" name="submit" class="btn btn-primary btn-block btnRound" style="margin-bottom: 10px !important;" id="register" value="Login">
+                    </form>	
+                </div>
             </div>
-            <div class="form-group"> 
-                <label class="form-control-placeholder" for="password">Password</label> 
-                <input type="password" name="Password" id="Password" class="form-control" value="" required> 
-                <i class="fa fa-lock iconSet" aria-hidden="true"></i>
-            </div> 
-             
-        </div> 
-        <input type="submit" name="submit" class="btn btn-primary btn-block" style="margin-bottom: 10px !important" id="register" value="Login">
-    </form>	
-</div>
+
+
+        </div>
+    </div>
+</section>
 
 @endsection
 
